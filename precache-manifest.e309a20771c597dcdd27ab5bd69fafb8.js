7f06005f1e1fb1b76aaee20ce6b57da3cfbd117d
afroz9008 <54906553+afroz9008@users.noreply.github.com>
2020-09-30 10:41:21 -0700

    Add files via upload

---
+++ precache-manifest.e309a20771c597dcdd27ab5bd69fafb8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b66bea9aaaa3bb87b97012ac50b48d",
+    "url": "/React-Posts/index.html"
+  },
+  {
+    "revision": "bba1d0e0ec86d35d97a4",
+    "url": "/React-Posts/static/css/3.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "45350806c8c36ef3758a",
+    "url": "/React-Posts/static/css/main.b79f8104.chunk.css"
+  },
+  {
+    "revision": "44e6f616b7a35cb811ea",
+    "url": "/React-Posts/static/js/0.2c4d9864.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/React-Posts/static/js/0.2c4d9864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba1d0e0ec86d35d97a4",
+    "url": "/React-Posts/static/js/3.4519aa6d.chunk.js"
+  },
+  {
+    "revision": "973773e5181314044ab48c1d66674c05",
+    "url": "/React-Posts/static/js/3.4519aa6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8711915d087b83e5365",
+    "url": "/React-Posts/static/js/4.5a680a18.chunk.js"
+  },
+  {
+    "revision": "1991a26e9a9f79c7cf22",
+    "url": "/React-Posts/static/js/5.f1d784c2.chunk.js"
+  },
+  {
+    "revision": "c40bcc9c1794474490bc",
+    "url": "/React-Posts/static/js/6.bc2453ab.chunk.js"
+  },
+  {
+    "revision": "7d3d8f15dd12770a320e",
+    "url": "/React-Posts/static/js/7.1ee9d53d.chunk.js"
+  },
+  {
+    "revision": "97dbf325028e7b3bcc0d",
+    "url": "/React-Posts/static/js/8.aed618ce.chunk.js"
+  },
+  {
+    "revision": "45350806c8c36ef3758a",
+    "url": "/React-Posts/static/js/main.42b92b55.chunk.js"
+  },
+  {
+    "revision": "754eb5c413f9a118ad51",
+    "url": "/React-Posts/static/js/runtime-main.0cd3970a.js"
+  }
+]);
---
+++ static/js/8.aed618ce.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfunction-component"]=this["webpackJsonpfunction-component"]||[]).push([[8],{137:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return o}));var i=e(0),c=e.n(i);function o(n){var t=n.url,e=n.id;return c.a.createElement("img",{src:t+"?text="+e,className:"img-fluid img",alt:"Something went wrong"})}}}]);
+//# sourceMappingURL=8.aed618ce.chunk.js.map
